853ff4d1fdd8db1acd6848cc9a41ff1f7226fd15
CrowdSense <admin@DevBook.home>
2010-01-17 20:51:29 +0000

    updated htmllib5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = [
     'Django==1.1.1',
     'markdown2==1.0.1.16',
     'python-openid==2.2.4',
-    'html5lib==0.11.1',
+    'html5lib==0.90',
     'django-extensions==0.4.1',
 
     'app_media',
